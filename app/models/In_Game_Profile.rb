1cecce7ff8d4b3110dd4b98d257a19f050078c89
Pablo G <pablo-shadow@pablo-g.-shadow>
2014-12-01 00:06:25 +0100

    Retouching models

--- app/models/In_Game_Profile.rb
+++ app/models/In_Game_Profile.rb
@@ -6,16 +6,22 @@ class InGameProfile < ActiveRecord::Base
 
 	validates :big_blinds, presence: true
 	#Validate that is data type: float, also used numericality?
-	validates :big_blind, numericality: true
+	validates :big_blinds, numericality: true
 
-	validates :small_blind, presence: true
+	validates :small_blinds, presence: true
 	#Validate that is data type: float, also used numericality?
-	validates :small_blind, numericality: true
+	validates :small_blinds, numericality: true
 
 	validates_inclusion_of :my_turn => [true, false]
 
-	validates_inclusion_of :my_win => [true, false]#
+	validates_inclusion_of :turn_bb => [true, false]
 
-	validates_inclusion_of :turn_time => [true, false]#
+	validates_inclusion_of :turn_sb => [true, false]
+
+	validates_inclusion_of :turn_others_positions => [true, false]
+
+	validates_inclusion_of :my_win => [true, false]
+
+	validates_inclusion_of :turn_time => [true, false]
 
 end
--- db/migrate/In_Game_Profile.rb
+++ db/migrate/In_Game_Profile.rb
@@ -6,15 +6,18 @@ class InGameProfile < ActiveRecord::Migration
     	
     	t.float :big_blinds, null: false, default: 0
 
+        t.float :small_blinds, null: false, default: 0
+
         ###The next field would be to ask if it's my turn.
-    	t.boolean :my_turn, #########
+    	t.boolean :my_turn #########
 
         ###The following fields would be to ask whether the duty to pay the big blind and small blind player.
-        t.boolean :turn_bb, #########
-        t.boolean :turn_sb, #########
+        t.boolean :turn_bb #########
+        t.boolean :turn_sb #########
+        t.boolean :turn_others_positions #########
 
         ###The next field would be to ask if I won.
-    	t.boolean :my_win, #########
+    	t.boolean :my_win #########
 
     	t.datetime :turn_time, null: false, default: 60 #60 seconds
 
